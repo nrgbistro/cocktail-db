a3d0bfd213bf830ee6e31b4332dd7d53a07d06ab
nrgbistro <nolangelinas@gmail.com>
2022-09-28 00:56:46 -0400

    Convert to next

--- src/components/search/components/SearchBar.tsx
+++
@@ -1,5 +0,0 @@
-const SearchBar = () => {
-	return <div className="">SearchBar</div>;
-};
-
-export default SearchBar;
--- src/components/search/components/Title.tsx
+++
@@ -1,9 +0,0 @@
-const Title = () => {
-	return (
-		<div>
-			<h1 className="text-4xl font-bold">Cocktail DB</h1>
-		</div>
-	);
-};
-
-export default Title;
