22028ed774816afd83856b7d8cf02dc207d77868
nrgbistro <nolangelinas@gmail.com>
2022-09-28 00:15:02 -0400

    Restructure components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import BasicSearch from "./components/search/BasicSearch";
+
 function App() {
-	return <div className="w-screen h-screen"></div>;
+	return (
+		<div className="w-screen h-screen flex flex-col justify-center">
+			<BasicSearch />
+		</div>
+	);
 }
 
 export default App;
