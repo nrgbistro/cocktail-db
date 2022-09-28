2d9a489a43de7d86ef9e7c089ecfd70168f90bf9
nrgbistro <nolangelinas@gmail.com>
2022-09-28 01:24:03 -0400

    Add formik example

--- components/search/components/SearchBar.tsx
+++ components/search/components/SearchBar.tsx
@@ -1,7 +1,38 @@
+import { Formik, Field, Form } from "formik";
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
 const SearchBar = () => {
 	return (
 		<div className="">
-			<form></form>
+			<Formik
+				initialValues={{
+					firstName: "",
+					lastName: "",
+					email: "",
+				}}
+				onSubmit={async (values) => {
+					await sleep(500);
+					alert(JSON.stringify(values, null, 2));
+				}}
+			>
+				{({ isSubmitting }) => (
+					<Form>
+						<label htmlFor="firstName">First Name</label>
+						<Field name="firstName" placeholder="Jane" />
+
+						<label htmlFor="lastName">Last Name</label>
+						<Field name="lastName" placeholder="Doe" />
+
+						<label htmlFor="email">Email</label>
+						<Field name="email" placeholder="jane@acme.com" type="email" />
+
+						<button type="submit" disabled={isSubmitting}>
+							Submit
+						</button>
+					</Form>
+				)}
+			</Formik>
 		</div>
 	);
 };
